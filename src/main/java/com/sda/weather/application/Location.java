9dc137b32ed404e46ba9039b5eba9754ab8a9071
adixes13 <adixes13@gmail.com>
2020-10-18 11:18:43 +0200

    Initial commit

---
+++ src/main/java/com/sda/weather/application/Location.java
@@ -0,0 +1,10 @@
+package com.sda.weather.application;
+
+public class Location {
+
+    private String cityName;
+    private String regionName;
+    private String countryName;
+    private double latitude;
+    private double longitude;
+}
---
+++ src/main/java/com/sda/weather/client/Client.java
@@ -0,0 +1,60 @@
+package com.sda.weather.client;
+
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Witaj w programie WeatherApp");
+        while (true) {
+            System.out.println("Wybierz opcję, co chcesz zrobić?");
+            System.out.println("1. Wprowadź swoje dane");
+            System.out.println("2. Wyświetl dane meteorologiczne w swoim regionie");
+            System.out.println("0. Zamknij aplikację");
+            int response = scanner.nextInt();
+            switch (response) {
+                case 1:
+                    addPersonalData();
+                    break;
+                case 2:
+                    readWeather();
+                    break;
+                case 0:
+                    System.out.println("Twoja aplikacja jest zamknięta");
+                    return;
+            }
+        }
+    }
+
+    private static void addPersonalData() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj miasto");
+        String cityName = scanner.nextLine();
+        System.out.println("Podaj region");
+        String regionName = scanner.nextLine();
+        System.out.println("Podaj nazwę kraju");
+        String countryName = scanner.nextLine();
+        System.out.println("Podaj szerokość geograficzną -180 W, 180 E");
+        double latitude = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Podaj długość geograficzną -90 S, 90 N");
+        double longitude = scanner.nextDouble();
+        scanner.nextLine();
+
+
+        System.out.println();
+
+    }
+
+    private static void readWeather() {
+        String response = entryController.readWeather();
+        System.out.println("Twoja pogoda: " + response);
+
+    }
+
+
+}
+
+
